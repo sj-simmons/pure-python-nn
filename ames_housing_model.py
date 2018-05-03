da445f916040e8d0f805e790d548c7850fd047dd
Chris Branton <cbranton@drury.edu>
2018-05-03 18:33:23 -0500

    Walking through the code shows no obvious problems, but the two order of magnitude error persists. Could it be the bias nodes. I may try adding a hidden layer. The ames_housing_model.py could work as the basis for an assignment.

--- ames_housing_model.py
+++ ames_housing_model.py
@@ -4,6 +4,30 @@ import csv
 from SGD_nn import Net
 from Pure_Python_Stats import mean_center, normalize, un_map_weights
 
+def test(test_csv_path, net):
+    # This code block reads the data from the csv file and, skipping the first line, writes the
+    # 1st through 14th elements of each line to appropriate lists.
+    with open(test_csv_path) as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        labels = []
+        labels = next(csvfile).split(',')  # Skip the first line of csvfile that holds the labels.
+        xs = []  # Create empty lists to hold the inputs,
+        ys = []  # and the outputs.
+        for row in reader:
+            xs.append([float(row[i]) for i in range(1, 14)])  # The 2nd through 13th entries are the inputs of
+            # each example.
+            ys.append(float(row[0]))  # The 1st entry of each line is the corresponding output.
+
+        # Add a column of ones to the inputs so that the neural net has a bias term.
+        xs = [[1] + x for x in xs]
+
+        for i in range(len(xs)):
+            net.forward(xs[i])
+            out = net.getOutput()
+#            for j in range (len(out)):
+            err = ys[i] - out #[j]
+            print('Predicted: {1}\tActual: {0}\tError: {2}\n'.format(ys[i], out, err))
+
 # This code block reads the data from the csv file and, skipping the first line, writes the
 # 1st through 14th elements of each line to appropriate lists.
 with open('ames_small.csv') as csvfile:
@@ -43,13 +67,13 @@ def printloss(loss, idx, epochs, num_last_lines = 0):
     else:
         print('current loss: {0:12f}'.format(loss))
 
-epochs = 10
+epochs = 100
 learning_rate = 0.03
 
 for i in range(epochs):  # train the neural net
     for j in range(len(xs)):
         net.learn(xs[j], ys[j], learning_rate)
-    printloss(net.getTotalError(), i, epochs)
+    printloss(net.getTotalError(xs, ys), i, epochs)
 
 # Get the weights from the trained model.
 weights = net.getWeights() # Weights is list of length 14 for these data.
@@ -66,3 +90,6 @@ print ("\nWhere:\n "+\
 
 #print("The actual least squares regression plane is:"+\
 #                                    "                  -11371.838 + 1.147*x1 + 8.047*x2.")
+
+print ('Testing with ames_small.csv')
+test('ames_small.csv', net)
