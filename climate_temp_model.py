7afd4a609de10e011173829da24ba55f105f10de
Scott Simmons <ssimmons1331@gmail.com>
2019-03-17 11:29:15 -0500

    fixed var names; tweaked output

--- climate_temp_model.py
+++ climate_temp_model.py
@@ -10,29 +10,29 @@ from liststats import mean_center, normalize, un_map_weights
 with open('datasets/temp_co2_data.csv') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     next(csvfile)  # Skip the first line of csvfile that holds the labels.
-    xs = [] # Create empty lists to hold the inputs,
-    ys = [] # and the outputs.
+    xss = [] # Create empty lists to hold the inputs,
+    yss = [] # and the outputs.
     for row in reader:
-        xs.append([float(row[2]), float(row[3])]) # The 3rd and 4th entries are
+        xss.append([float(row[2]), float(row[3])]) # The 3rd and 4th entries are
                                                    # the inputs of each example.
-        ys.append([float(row[1])])  # The 2nd entry of each line is the corres-
+        yss.append([float(row[1])])  # The 2nd entry of each line is the corres-
                                      # ponding output.
 # Note: the xss and yss are already list of lists, as we need them to be to use
 #        the Net class feedforwardnn.py
 
 # Now mean_center and normalize the data, saving the means and standard
 # deviations of both input variable and the output variable.
-xmeans, xs = mean_center(xs)
-xstdevs, xs = normalize(xs)
-ymeans, ys = mean_center(ys)
-ystdevs, ys = normalize(ys)
+xmeans, xss = mean_center(xss)
+xstdevs, xss = normalize(xss)
+ymeans, yss = mean_center(yss)
+ystdevs, yss = normalize(yss)
 
-# An instance of Net() which accepts 2 inputs and 1 output and mean squared error for the
-# criterion.
+# An instance of Net() which accepts 2 inputs and 1 output, and mean squared
+# error for the loss.
 net = Net([2,1], activations = [None], loss = 'MSE')
 print(net)
 
-train(net, xs, ys, batchsize=32, epochs=100, learning_rate=0.1, prtlns=20)
+train(net, xss, yss, batchsize=32, epochs=100, learning_rate=0.1)
 
 # Get the weights from the trained model.
 weights = [net.forward([[1, 0]])[0][0], net.forward([[0, 1]])[0][0]]
@@ -41,9 +41,12 @@ weights = [net.forward([[1, 0]])[0][0], net.forward([[0, 1]])[0][0]]
 weights = un_map_weights(weights, xmeans, xstdevs, ymeans, ystdevs)
 
 # Print out the plane found by the neural net.
-print("\nThe least squares regression plane found by the neural net is: "+\
-        "{0:.3f} + {1:.3f}*x1 + {2:.3f}*x2".format(weights[0], weights[1], weights[2]), end='')
+print("\nThe least squares regression plane found by the neural net is:\n"+\
+        "     {0:.3f} + {1:.3f}*x1 + {2:.3f}*x2".\
+        format(weights[0], weights[1], weights[2]), end='')
 print(", where x1 is CO2 and x2 is SolarIrr.")
 
-print("The actual least squares regression plane is:" + " " * 18 +\
-                                                           "-11371.838 + 1.147*x1 + 8.047*x2.")
+# To check how well the neural net did, here's the actual least-squares
+# regression plane (found by solving the normal equations).
+print("The actual least squares regression plane is:\n" + " " * 5 +\
+      "-11371.838 + 1.147*x1 + 8.047*x2.")
